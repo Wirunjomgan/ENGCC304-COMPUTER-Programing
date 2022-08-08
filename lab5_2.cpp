c444508319a02707cc65fb7f717d54d482c5527e
_wirunx <108041256+Wirunjomgan@users.noreply.github.com>
2022-08-09 03:04:13 +0700

    05

---
+++ lab5_2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	
+    int years ;
+    printf( "Input years : " ) ;
+    scanf( "%d", &years ) ;
+    years = years % 4 ;
+    
+    switch (years) {
+        case 0 :
+            printf( "Feb = 29 days" ) ;
+        break ;
+        default:
+            printf( "Feb = 28 days" ) ;
+        break ;
+   }
+    return 0 ;
+}
